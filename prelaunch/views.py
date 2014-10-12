576de1309b852cef4febe3b46de882e09ca1ccfd
Ethan Gottlieb <ethan@dormserv.com>
2014-10-12 17:46:04 -0400

    add html for homepage

---
+++ prelaunch/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.shortcuts import render_to_response, redirect
+from django.template import RequestContext
+
+def home(request):
+	context = RequestContext(request)
+
+	return render_to_response(
+		'prelaunch_home.html',
+		{},
+		context)
